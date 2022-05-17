13f2d7eb915e72b2443f541001f543971ff00279
Kaegan O'Hara <kohara321@gmail.com>
2022-05-17 09:46:39 -0600

    commit tree 171301664ffd1fd8c189ac340d8864437a3e1f45

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks > 42) {
+        return blocks - 42
+    }
+    else {
+        return 42 - blocks
+    }
+}
+
+function distanceFromHqInFeet(blocks) {
+   return distanceFromHqInBlocks(blocks) * 264;
+}
+
+function distanceTravelledInFeet(blocks, feet) {
+    if (feet < blocks) {
+        return (blocks - feet) * 264
+    }
+    else {
+        return (feet - blocks) * 264
+    }
+}
+
+function calculatesFarePrice(blocks, feet) {
+    const distance = distanceTravelledInFeet(blocks, feet)
+        if (distance <= 400) {
+            return 0
+     }
+        else if (distance > 400 && distance <=2000) {
+            return .02 * (distance - 400)
+     }
+        else if (distance > 2000 && distance < 2500)
+            return 25
+        else {
+            return 'cannot travel that far'
+     }
+}
